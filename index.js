d1e00eca656556865c1cac1dc949ed4c43a808d1
Stanley Szeto <sszeto@uwaterloo.ca>
2020-11-15 18:49:30 -0800

    setup react pdf rendering

--- index.js
+++ index.js
@@ -1,56 +1,34 @@
 'use strict';
-console.log('Loading hello world function');
- 
-exports.handler = async (event) => {
-    let name = "you";
-    let city = 'World';
-    let time = 'day';
-    let day = '';
-    let responseCode = 200;
-    console.log("request: " + JSON.stringify(event));
-    
-    if (event.queryStringParameters && event.queryStringParameters.name) {
-        console.log("Received name: " + event.queryStringParameters.name);
-        name = event.queryStringParameters.name;
-    }
-    
-    if (event.queryStringParameters && event.queryStringParameters.city) {
-        console.log("Received city: " + event.queryStringParameters.city);
-        city = event.queryStringParameters.city;
-    }
-    
-    if (event.headers && event.headers['day']) {
-        console.log("Received day: " + event.headers.day);
-        day = event.headers.day;
-    }
-    
-    if (event.body) {
-        let body = JSON.parse(event.body)
-        if (body.time) 
-            time = body.time;
-    }
- 
-    let greeting = `Good ${time}, ${name} of ${city}.`;
-    if (day) greeting += ` Happy ${day}!`;
+require("@babel/register")({
+	presets: ["@babel/preset-env", "@babel/preset-react"]
+});
 
-    let responseBody = {
-        message: greeting,
-        input: event
-    };
+const ReactPDF = require('@react-pdf/renderer');
+const { TestPdf } = require('./pdfs/testPdf');
+
+// exports.handler = async (event) => {
+//     let greeting = `Good ${time}, ${name} of ${city}.`;
+//     if (day) greeting += ` Happy ${day}!`;
+
+//     let responseBody = {
+//         message: greeting,
+//         input: event
+//     };
     
-    // The output from a Lambda proxy integration must be 
-    // in the following JSON object. The 'headers' property 
-    // is for custom response headers in addition to standard 
-    // ones. The 'body' property  must be a JSON string. For 
-    // base64-encoded payload, you must also set the 'isBase64Encoded'
-    // property to 'true'.
-    let response = {
-        statusCode: responseCode,
-        headers: {
-            "x-custom-header" : "my custom header value"
-        },
-        body: JSON.stringify(responseBody)
-    };
-    console.log("response: " + JSON.stringify(response))
-    return response;
-};
+//     let response = {
+//         statusCode: responseCode,
+//         headers: {
+//             "x-custom-header" : "my custom header value"
+//         },
+//         body: JSON.stringify(responseBody)
+//     };
+//     return response;
+// };
+
+const handler = async (event) => {
+	ReactPDF.render(TestPdf, `./test.pdf`);
+}
+
+handler();
+
+exports.handler = handler;
---
+++ pdfs/testPdf.js
@@ -0,0 +1,29 @@
+const React = require('react');
+const { Page, Text, View, Document, StyleSheet } = require('@react-pdf/renderer');
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+// Create Document Component
+exports.TestPdf = (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
