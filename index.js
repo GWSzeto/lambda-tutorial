c37eeb542cf1b7f35412a60a67b424c6294c9270
Stanley Szeto <sszeto@uwaterloo.ca>
2020-11-17 00:11:47 -0500

    minimum repro

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ const ReactPDF = require('@react-pdf/renderer');
 // const { TestPdf } = require('./pdfs/testPdf');
 
 exports.handler = async (event) => {
-		const greeting = "Heyooo world"
+	console.log("Hello World!")
+		// const greeting = "Heyooo world"
 
-    let responseBody = {
-        message: greeting,
-        input: event
-    };
+    // let responseBody = {
+    //     message: greeting,
+    //     input: event
+    // };
     
-    let response = {
-        statusCode: 200,
-        headers: {
-            "x-custom-header" : "my custom header value"
-        },
-        body: JSON.stringify(responseBody)
-    };
-    return response;
+    // let response = {
+    //     statusCode: 200,
+    //     headers: {
+    //         "x-custom-header" : "my custom header value"
+    //     },
+    //     body: JSON.stringify(responseBody)
+    // };
+    // return response;
 };
 
 // const handler = async (event) => {
