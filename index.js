5f059abb12312ab15b32cb1204e599e4cf575b17
Stanley Szeto <sszeto@uwaterloo.ca>
2020-11-18 02:08:14 -0500

    set up postgres

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 'use strict';
+require('dotenv').config()
 require("@babel/register")({
 	presets: ["@babel/preset-env", "@babel/preset-react"]
 });
+const { Pool } = require('pg')
 
 const ReactPDF = require('@react-pdf/renderer');
 const { TestPdf } = require('./pdfs/testPdf');
 
-exports.handler = async (event) => {
-		const greeting = "Heyooo world"
+console.log("user: ", process.env.USERNAME)
+const pool = new Pool({
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	user: process.env.USERNAME,
+	password: process.env.PASSWORD,
+	port: process.env.PORT,
+})
 
-    let responseBody = {
-        message: greeting,
-        input: event
-    };
-    
-    let response = {
-        statusCode: 200,
-        headers: {
-            "x-custom-header" : "my custom header value"
-        },
-        body: JSON.stringify(responseBody)
-    };
-    return response;
+const handler = async (event) => {
+	const { rows } = await pool.query("SELECT * FROM patient WHERE first_name = 'asd'");
+
+	let responseBody = {
+		message: rows,
+		input: event
+	};
+	
+	let response = {
+		statusCode: 200,
+		headers: {
+				"x-custom-header" : "my custom header value"
+		},
+		body: JSON.stringify(responseBody)
+	};
+	return response;
 };
+
+handler()
+
+exports.handler = handler;
 
 // const handler = async (event) => {
 // 	ReactPDF.render(TestPdf, `./test.pdf`);
